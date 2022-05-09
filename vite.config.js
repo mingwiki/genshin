719242b3bcaa5d4683dafff2f324cfd5999109a7
mingwiki <mingwiki@gmail.com>
2022-05-09 16:38:19 +0800

    update icons

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,40 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://zed.ink/genshin",
-  plugins: [react(), VitePWA({})],
+  base: "https://zed.ink/genshin/",
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        "./favicon.svg",
+        "./favicon.ico",
+        "./robots.txt",
+        "./apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "原神助手",
+        short_name: "原神助手",
+        description: "原神小工具",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "./pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
