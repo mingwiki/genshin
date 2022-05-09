01339861917821c748bee846defa5e200ea1fc37
mingwiki <mingwiki@gmail.com>
2022-05-09 17:11:04 +0800

    change nothing its stupid

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
     react(),
     VitePWA({
       includeAssets: [
-        "./favicon.svg",
-        "./favicon.ico",
-        "./robots.txt",
-        "./apple-touch-icon.png",
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
       ],
       manifest: {
         name: "原神助手",
@@ -21,17 +21,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "./android-chrome-192x192.png",
+            src: "android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "./android-chrome-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "./android-chrome-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
