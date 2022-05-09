2a1d0bddbf70a7c073b7a00b29dafc9d51a81e8d
mingwiki <mingwiki@gmail.com>
2022-05-09 17:04:15 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { registerSW } from "virtual:pwa-register";
 
-const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
-});
-updateSW()
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
