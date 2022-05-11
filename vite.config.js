159012468cc8ab7938af6daeca22c0027ca6333a
mingwiki <mingwiki@gmail.com>
2022-05-11 14:25:56 +0800

    update webmanifest

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "https://zed.ink/genshin/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        cleanupOutdatedCaches: true,
+        sourcemap: true,
+      },
+    }),
+  ],
 });
